ca1c44af728efabc4654b45f10c1fdeccb790baf
Nikhil Wakode <wakode333nikhil@gmail.com~>
2025-04-01 03:26:12 +0530

    Initial Setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+         <h1 className="text-blue-800 p-4 text-center">Lets Start Building something cool....</h1>
+          <p className="text-red-600 p-4 text-center">Cooking...</p>
+    </>
+  )
+}
+
+export default App
